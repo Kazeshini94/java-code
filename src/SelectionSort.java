22c855c46a4c99d6c41ca28f79fea78f955fa161
Kazeshini94 <Huemer94@gmx.at>
2022-06-13 14:07:59 +0200

    Finished the first Sorting-Algorithms

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,5 +1,38 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
 public class SelectionSort {
     public static void main(String[] args) {
 
+        Random rng = new Random();
+        ArrayList<Integer> usedNumber = new ArrayList<>();
+        int[] input = new int[100000];
+        int roll, len = input.length;
+
+        // Declare Variables in the Array
+        for (int i = 0; i < len; i++) {
+            do {
+                roll = rng.nextInt(0, input.length * 10);
+            }
+            while (usedNumber.contains(roll));
+            usedNumber.add(roll);
+            input[i] = roll;
+        }
+
+        long startTime = System.currentTimeMillis();
+        
+        // Sorting starts here
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if (input[i] > input[j]) {
+                    int check = input[i];
+                    input[i] = input[j];
+                    input[j] = check;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(input));
+        System.out.println((System.currentTimeMillis() - startTime));
     }
 }
---
+++ src/getAverage.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class getAverage {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Wieviele Noten gibt es zu Bewerten?");
+        int result=0,length = scanner.nextInt();String text="";
+        int[] array =  new int[length];
+        for (int i = 0; i < length; i++) {
+            System.out.print("Note: ");
+            array[i]= scanner.nextInt();
+            result+= array[i];
+        }
+        System.out.printf("%s %.2f","Notendurchschnitt: ",(double)result/array.length);
+    }
+}
