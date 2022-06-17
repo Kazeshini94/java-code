0c977278e2808038e481673f4d7bd5ee26571e48
Kazeshini <Huemer94@gmx.at>
2022-06-17 19:37:12 +0200

    little update

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -5,7 +5,7 @@ public class InsertionSort {
         Random rng = new Random(3);
         ArrayList<Integer> usedNumber = new ArrayList<>();
 
-        int[] input = new int[100000];
+        int[] input = new int[10000];
         int len = input.length;
         int roll;
 
