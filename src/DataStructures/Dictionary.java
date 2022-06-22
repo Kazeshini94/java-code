6016f702f981ab1b18d0ba2ff4efcfeb1e9f225b
Kazeshini <Huemer94@gmx.at>
2022-06-22 20:27:46 +0200

    Using Packages for the Code to keep it simply for overview

---
+++ src/DataStructures/Dictionary.java
@@ -0,0 +1,41 @@
+package DataStructures;
+
+import java.util.*;
+
+public class Dictionary {
+
+    static Map<String, String> dictionaryDeToEn = new HashMap<>();
+    static Map<String, String> dictionaryEnToDe = new HashMap<>();
+
+    public static void main(String[] args) {
+
+        add("Schule","School");
+        add("Katze","Cat");
+        add("Arbeitsplatz","Workplace");
+        delete("Schule","School");
+
+        System.out.println("German to English: " + dictionaryDeToEn);
+        System.out.println("English to German: " + dictionaryEnToDe);
+    }
+    public static void delete(String german, String english) {
+        dictionaryEnToDe.remove(english,german);
+        dictionaryDeToEn.remove(german,english);
+    }
+    public static void add(String german, String english) {
+        dictionaryDeToEn.put(german,english);
+        dictionaryEnToDe.put(english,german);
+    }
+}
+
+//        // Input for the Dictionary
+//        ArrayList<String> german = new ArrayList<>
+//                (Arrays.asList("Schule", "Katze", "Arbeitsplatz", "Hund", "Spiele", "Karten", "Scherz"));
+//        ArrayList<String> english = new ArrayList<>
+//                (Arrays.asList("School", "Cat", "Workplace", "Dog", "Games", "Cards", "Joke"));
+
+//        for (int i = 0; i < german.size() && i < english.size(); i++) {
+//            dictionaryDeToEn.put(german.get(i), english.get(i));
+//            if (dictionaryDeToEn.containsKey(german.get(i))) {
+//                dictionaryEnToDe.put(english.get(i), german.get(i));
+//            }
+//        }
---
+++ src/DataStructures/MergeSort.java
@@ -0,0 +1,75 @@
+package DataStructures;
+
+import java.util.*;
+
+public class MergeSort {
+    public static void main(String[] args) {
+
+        Random rng = new Random();
+        ArrayList<Integer> usedNumber = new ArrayList<>();
+        long startTime;
+        List<Integer> list = new ArrayList<>();
+        int counter = 0;
+
+        startTime = System.currentTimeMillis();
+        mergeSort(list);
+        System.out.println(list);
+        System.out.println("Completed in: " + (System.currentTimeMillis() - startTime) + "ms!");
+    }
+
+    public static void mergeSort(List<Integer> list) {
+        if (list.size() <= 1) {
+            return;
+        }
+
+        int mid = list.size() / 2;
+        int counter = 0;
+
+        List<Integer> left = new ArrayList<>(mid);
+        List<Integer> right = new ArrayList<>(list.size() - mid);
+        for (int i = 0; i < list.size(); i++) {
+            if (i < mid) {
+                left.set(i, list.get(i));
+            } else {
+                right.set(counter, list.get(i));
+                counter++;
+            }
+        }
+        // Recursion until both Arrays are divided to size 1 !
+        mergeSort(left);
+        mergeSort(right);
+        // Merge Left and Right into list in right order!
+        merge(list, left, right);
+    }
+
+    public static void merge(List<Integer> array,List<Integer> left,List<Integer> right) {
+        // Integers to Pick the right Value from each Array!
+        int checkIndex = 0, checkL = 0, checkR = 0;
+        // Comparing Values in Both Sub-Arrays and placing it in the right position!
+        while (checkL < left.size() && checkR < right.size()) {
+            // Going from Low to High number!
+            if (left.get(checkL) < right.get(checkR)) {
+                array.set(checkIndex, left.get(checkL));
+                checkL++;
+            } else {
+                array.set(checkIndex, right.get(checkR));
+                checkR++;
+            }
+            checkIndex++;
+        }
+        // If only Left Array is left append!
+        while (checkL < left.size()) {
+            array.set(checkIndex, left.get(checkL));
+            checkIndex++;
+            checkL++;
+        }
+        // Same for Right Array until nothing left!
+        while (checkR < right.size()) {
+            array.set(checkIndex, right.get(checkR));
+            checkIndex++;
+            checkR++;
+        }
+    }
+}
+
+
