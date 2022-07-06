2a6f669e27e75a81bae2823b4ec76e0ef17ef27f
Kazeshini94 <105610471+Kazeshini94@users.noreply.github.com>
2022-07-06 15:32:40 +0200

    Update Main.java

--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -1,5 +1,4 @@
-
-  package LinkedList;
+package LinkedList;
 
 public class Main {
     public static void main(String[] args) {
@@ -32,7 +31,6 @@ public class Main {
 //
 //        System.out.println("----END-3---");
 
-<<<<<<< HEAD
         list.removeFirst();
         list.append("First");
         list.append("Second");
@@ -58,45 +56,5 @@ public class Main {
         // list.append("Fifth")
 
         // Output printList -> Fourth, Third, Second, First, Fifth
-=======
-        // To Show functionality of created Methods!
-//        System.out.println("----START----");
-//        list.push("Surprise");
-//        list.append("End of Fun!");
-//        list.push("PUSH-TEST");
-//        list.printList();
-//
-//        System.out.println("----END-1---");
-//
-//        System.out.println(list.getValue("Head"));
-//        System.out.println(list.get(5));
-//        list.removeFirst();
-//        list.removeLast();
-//        list.printList();
-//
-//        System.out.println("----END-2---");
-
-        // To Show Removal of the Full List!
-//        list.remove();
-//        list.size();
-//        list.printList();
-//
-//        System.out.println("----END-3---");
-
-        list.removeFirst();
-        list.append("One");
-        list.append("Two");
-        list.append("Three");
-        list.append("Four");
-
-        list.printList();
-
-        System.out.println("-----------");
-        list.reverseRecursive(list.size());
-        list.printList();
-
-
-
->>>>>>> 8b3ec42d6e9c35e06df154de39f809e833fcf5ee
     }
 }
